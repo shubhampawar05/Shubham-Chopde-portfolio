1986159d86fa006187a1892a249b919f8acddd00
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-12 11:11:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# [Shubham Chopde](https://kjcreddy666.github.io/portfolio/)
+
+![Shubham Chopde's Portfolio-image](src/images/portfolio.png)
+
+
+🚀 Welcome to my latest portfolio project! 🚀
+
+This project is a showcase of my skills and projects, developed using HTML5, CSS3, JavaScript, and powered by React.js.
+
+
+## Technologies Used:
+
+- HTML5
+- CSS3
+- JavaScript
+- ReactJS
+
+## Getting Started:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Install dependencies using `npm install`.
+4. Start the development server using `npm start`.
+5. Open your browser and visit `http://localhost:3000` to view the portfolio.
+
+## Let's Connect:
+
+Feel free to reach out via email at [shubhamchopde090@gmail.com](mailto:your.email@example.com) or connect with me on [LinkedIn](https://www.linkedin.com/in/shubham-chopde05/).
+
---
+++ src/data/constants.js
@@ -0,0 +1,266 @@
+export const Bio = {
+    name: "Shubham Chopde",
+    roles: [
+      "Frontend Developer",
+      "Programmer",
+    ],
+    description: "To kickstart my career as a Frontend Developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+    github: "https://github.com/shubhampawar05",
+    resume: "https://drive.google.com/file/d/1R1Syqj6KJulM7voZk2zBJxS0yjNacUaO/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/shubham-chopde05/",
+    twitter: "https://twitter.com/",
+    insta: "https://www.instagram.com/shubham_05_11/",
+    facebook: "https://www.facebook.com/shubhamc1250/",
+};
+  
+export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        },
+        {
+          name: "JavaScript",
+          image: "Python-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "html5-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "CSS3-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "bootstrap-logo.png",
+        },
+        {
+          name: "Tailwind",
+          image:
+            "tail.svg",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "JavaScript-logo.png",
+        },
+        {
+          name: "React",
+          image:
+            "React.png",
+        }
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "nodejs-logo.svg",
+        },
+        {
+          name: "Express Js",
+          image: "express.png",
+        },
+        {
+          name: "Mongo DB",
+          image: "mogo.png",
+        },
+       
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "git1.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "ghub.svg",
+        },
+        {
+          name: "VS Code",
+          image:
+            "vscode-logo.png",
+        },
+      ],
+    },
+];
+  
+export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Docker",
+        "Terraform",
+        "AWS",
+        "EC2",
+        "Portainer",
+        "Nginx",
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+  
+];
+  
+export const education = [
+    {
+      id: 0,
+      img: "college.png",
+      school: "Dr. Annasaheb Shinde College of Agricultural Engineering and Technology, MPKV Rahuri ",
+      date: "2019 - 2023",
+      grade: "8.01 CGPA",
+      desc: "I graduated in Agricultural Engineering from Dr. Annasaheb Shinde College of Agricultural Engineering and Technology, MPKV Rahuri. The program equipped me with skills in farm machinery, soil and water conservation, agricultural processing, and renewable energy, preparing me to innovate and improve agricultural practices.",
+      degree: "Bachelor of Technology - BTech, Agricultural Engineering(AG)",
+    },
+    {
+      id: 1,
+      img: "jnvLogo.png",
+      school: "Jawahar Navodaya Vidyalaya, Singodi,Chhindwara",
+      date: "2017 - 2018",
+      grade: "72.60%",
+      desc: "I completed my Intermediate at Jawahar Navodaya Vidyalaya, Singodi where I studied  Physics, Chemistry and  Maths stream.",
+      degree: "Central Board of Secondary Education(CBSE)",
+    },
+    {
+      id: 2,
+      img: "jnvLogo.png",
+      school: "Jawahar Navodaya Vidyalaya, Singodi,Chhindwara",
+      date: "2016",
+      grade: "7.6 GPA",
+      desc: "I completed my class 10 education at Jawahar Navodaya Vidyalaya, Singodi.",
+      degree: "Central Board of Secondary Education(CBSE)",
+    },
+];
+  
+export const projects = [
+    {
+      id: 9,
+      title: "Ecommerce Admin Panel",
+      description:
+       "Developed a basic e-commerce website with a focus on robust admin panel functionalities using React and Firebase database integration. Implemented CRUD operations for admin users while providing seamless cart management and order placement for regular users. Utilized Tailwind CSS for responsive design and enhanced user experience.",
+      image:
+        "project.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJs"
+      ],
+      category: "web app",
+      github: "https://github.com/shubhampawar05/MCT-Project",
+      webapp: "https://mct-project.vercel.app/",
+    },
+
+    {
+      id: 9,
+      title: "Movix App",
+      description:
+        "I developed a dynamic Movie Catalogue App with React, offering a seamless experience for exploring movies and TV shows. Key features include carousel sections for popular, top-rated, and trending content, real-time updates from a movie database API, and interactive components displaying detailed movie information, cast, and official videos. Utilizing React's capabilities, the app provides a user-friendly interface, enhancing accessibility and engagement for movie enthusiasts.",
+      image:
+        "movix.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React",
+       
+      ],
+      category: "web app",
+      github: "https://github.com/shubhampawar05/NewMovixProject",
+      webapp: "https://react-movix-project-nine.vercel.app/",
+    },
+    {
+      id: 9,
+      title: "Portfolio",
+      description:
+        "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+      image:
+        "portfolio-logo.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React",
+      ],
+      category: "web app",
+      github: "https://github.com/kjcreddy666/portfolio",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "AxisBank Clone",
+      description:
+        'Welcome to our Axis Bank Website Clone project! This repository contains the front-end implementation of the Axis Bank website created as part of a Geekathon hosted by Geekster Reduced dependence on JavaScript through innovative use of HTML and CSS.',
+      image:
+        "axisbank.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        
+      ],
+      category: "web app",
+      github: "https://github.com/shubhampawar05/Major-Project-Axis-Bank",
+      webapp: "https://major-project-axis-bank.vercel.app/",
+    },
+
+    {
+      id: 9,
+      title: "YouTube Timeline Bookmarker",
+      description:
+        `"YouTube Timeline Bookmarker is a Chrome extension that serves as a convenient tool for bookmarking YouTube videos by saving specific intervals of the video timeline. It enables users to jump directly to these bookmarked intervals, facilitating the resumption of videos added to the "Watch Later" list or the creation of important notes during lectures. Additionally, users can easily bookmark the beginning of their favorite songs for quick access.`,
+      image:
+        "ytBookMark.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+      ],
+      category: "web app",
+      github: "https://github.com/shubhampawar05/Team-20/tree/main/Shubham%20Chopde/YouTube%20Chrome%20Extension",
+      webapp: "https://www.linkedin.com/posts/shubham-chopde05_youtubeextension-geekathon-chromeextension-activity-7163135123511615488-0Z_E?utm_source=share&utm_medium=member_desktop",
+    },
+
+    {
+      id: 9,
+      title: "Aria",
+      description:
+        "his project is a front-end clone of an ARIA website, built using HTML and CSS. It focuses on creating an accessible web interface that includes navigational elements, interactive components, and dynamic content. The design adheres to ARIA guidelines, ensuring that it is usable by individuals with disabilities, providing a more inclusive web experience.",
+      image:
+        "aria.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+      ],
+      category: "web app",
+      github: "https://github.com/shubhampawar05/MajorProjectAria",
+      webapp: "https://major-project-aria.vercel.app/",
+    },
+];
+  
